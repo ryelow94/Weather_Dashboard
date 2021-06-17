d154a1713949b2a23d64219633477b75a7713c8b
Ryan Logan <ryan_plogan@yahoo.com>
2021-06-16 18:40:35 -0700

    saves entries to local storage still need date/time and icon to work properly

--- assets/script.js
+++ assets/script.js
@@ -25,22 +25,26 @@ function getCurrentWeather(event) {
     var currentWeatherHumidity=document.createElement("h6")
     var currentWeatherWind=document.createElement("h6")
     var currentWeatherTitle=document.createElement("h2") 
+    var currentWeatherRightNow=document.createElement("h3")
     currentWeatherCard.classList= "card"
     currentWeatherBody.classList= "card-body" 
     currentWeatherTemp.classList="card-text"
     currentWeatherHumidity.classList="card-text"
     currentWeatherWind.classList="card-text"
+    currentWeatherRightNow.classList="card-text"
 
     currentWeatherTitle.textContent=data.name;
+    currentWeatherRightNow.textContent="Current Weather"
     currentWeatherTemp.textContent= data.main.temp + "°F"
-    currentWeatherHumidity.textContent=data.main.humidity + "%"
-    currentWeatherWind.textContent=data.wind.speed + "MPH"
-
+    currentWeatherHumidity.textContent=data.main.humidity + "%"+" humidity"
+    currentWeatherWind.textContent=data.wind.speed + "MPH"+" windspeed"
+    //creates the icon for weather display
     currentWeatherImg.setAttribute("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
 
     currentWeather.appendChild(currentWeatherCard)
     currentWeatherCard.appendChild(currentWeatherBody)
-    currentWeatherBody.appendChild(currentWeatherTitle) 
+    currentWeatherBody.appendChild(currentWeatherTitle)
+    currentWeatherBody.appendChild(currentWeatherRightNow) 
     currentWeatherBody.appendChild(currentWeatherTemp)
     currentWeatherTitle.appendChild(currentWeatherImg)
     currentWeatherBody.appendChild(currentWeatherHumidity) 
@@ -52,18 +56,76 @@ function getCurrentWeather(event) {
         //append this to list items
     }
 } 
-function getFiveDayForecast() {
+function getFiveDayForecast(event) {
+    event.preventDefault()
     var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+searchText.value+"&appid="+apiKey+"&units=imperial" 
     fetch(fiveDayUrl)
     .then(function(response) {
         return response.json()
     })
     .then(function(data) {
-        console.log(data)
+        console.log(data) 
+        for (let i = 0; i < data.list.length; i++) { 
+            var currentWeather=document.querySelector("#current-day")
+            //currentWeather.innerHTML=""
+            var currentWeatherCard=document.createElement("div")
+            var currentWeatherBody=document.createElement("div")
+            var currentWeatherImg= document.createElement("img")
+            var currentWeatherTemp=document.createElement("h6")
+            var currentWeatherHumidity=document.createElement("h6")
+            var currentWeatherWind=document.createElement("h6")
+            var currentWeatherTitle=document.createElement("h2") 
+            var currentWeatherDateAndTime=document.createElement("h3") 
+            currentWeatherCard.classList= "card"
+            currentWeatherBody.classList= "card-body" 
+            currentWeatherTemp.classList="card-text"
+            currentWeatherHumidity.classList="card-text"
+            currentWeatherWind.classList="card-text" 
+            currentWeatherDateAndTime.classList="card-text"
 
+            currentWeatherTitle.textContent=data.city.name;
+            currentWeatherDateAndTime.textContent=data.list[i].dt_txt
+            currentWeatherTemp.textContent= data.list[i].main.temp + "°F"
+            currentWeatherHumidity.textContent=data.list[i].main.humidity + "%"+" humidity"
+            currentWeatherWind.textContent=data.list[i].wind.speed + "MPH"+" windspeed"
+            //creates the icon for weatherdisplay
+            currentWeatherImg.setAttribute("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png")
 
+            currentWeather.appendChild(currentWeatherCard)
+            currentWeatherCard.appendChild(currentWeatherBody)
+            currentWeatherBody.appendChild(currentWeatherTitle)
+            currentWeatherBody.appendChild(currentWeatherDateAndTime) 
+            currentWeatherBody.appendChild(currentWeatherTemp)
+            currentWeatherTitle.appendChild(currentWeatherImg)
+            currentWeatherBody.appendChild(currentWeatherHumidity) 
+            currentWeatherBody.appendChild(currentWeatherWind) 
+            
+        }
     });
-}
+} 
+
+function appendLocalStorage() {
+    for (let i = 0; i < archive.length; i++) {
+        var ulEl=document.getElementById("local-storage") 
+        var liEl=document.createElement("li")
+        var button=document.createElement("button") 
+        button.setAttribute("data-city", archive[i])
+        button.textContent="search"
+        button.addEventListener("click",function(event) {
+            console.log(event.target.getAttribute("data-city"))
+            searchText.value=event.target.getAttribute("data-city")
+            getCurrentWeather(event)
+            getFiveDayForecast(event)
+        })
+        liEl.textContent=archive[i] 
+        ulEl.appendChild(liEl)
+        liEl.appendChild(button) 
+    }
+} 
+
+
+appendLocalStorage()
+
 searchButton.addEventListener("click", getCurrentWeather) 
 
 searchButton.addEventListener("click", getFiveDayForecast)
